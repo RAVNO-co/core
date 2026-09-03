549e32d58a0ee518ad4ae1437f34b3fb6c4a32fe
BeaverNotACat <beavernotacat@gmail.com>
2026-09-03 20:48:13 +1000

    fix: fix amount abstract definition

--- src/core/domain/value_objects/amount.py
+++ src/core/domain/value_objects/amount.py
@@ -1,97 +1,51 @@
-from __future__ import annotations
-
+from abc import ABC
 from decimal import Decimal
 from typing import Self, override
 
 from core.domain.exceptions import NegativeOrZeroAmountError
 
-_DecimalInput = Decimal | int | str
+_NewDecimal = Decimal | float | str
+_Decimal = Decimal | int
 
 
-class _PositiveDecimal:
-    """Immutable positive, finite decimal value object."""
+class _AmountABC(Decimal, ABC):
+    """Represents existing amount of smth
 
-    __slots__: tuple[str] = ("_value",)
+    Rewrites returned Decimal types
+    Cant be less or equal 0
+    """
 
-    def __init__(
-        self,
-        value: _DecimalInput,
+    def __new__(
+        cls,
+        value: _NewDecimal,
         prev: Self | None = None,
-    ) -> None:
-        value = Decimal(value)
-
-        if not value.is_finite() or value <= 0:
+    ) -> Self:
+        if float(value) <= 0:
             raise NegativeOrZeroAmountError(prev, value)
-
-        self._value: Decimal = value
-
-    @property
-    def value(self) -> Decimal:
-        return self._value
-
-    def __add__(self, other: Self) -> Self:
-        self._ensure_same_type(other)
-        return type(self)(self._value + other._value, prev=self)
-
-    def __sub__(self, other: Self) -> Self:
-        self._ensure_same_type(other)
-        return type(self)(self._value - other._value, prev=self)
-
-    def __mul__(self, other: _DecimalInput) -> Self:
-        return type(self)(self._value * Decimal(other), prev=self)
-
-    def __rmul__(self, other: _DecimalInput) -> Self:
-        return self * other
-
-    def __truediv__(self, other: _DecimalInput) -> Self:
-        return type(self)(self._value / Decimal(other), prev=self)
+        return super().__new__(cls, value)
 
     @override
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, type(self)):
-            return NotImplemented
-
-        return self._value == other._value
-
-    def __lt__(self, other: Self) -> bool:
-        self._ensure_same_type(other)
-        return self._value < other._value
-
-    def __le__(self, other: Self) -> bool:
-        self._ensure_same_type(other)
-        return self._value <= other._value
-
-    def __gt__(self, other: Self) -> bool:
-        self._ensure_same_type(other)
-        return self._value > other._value
-
-    def __ge__(self, other: Self) -> bool:
-        self._ensure_same_type(other)
-        return self._value >= other._value
+    def __add__(self, value: Decimal | int, /) -> Self:
+        return self.__new__(self.__class__, super().__add__(value))
 
     @override
-    def __hash__(self) -> int:
-        return hash((type(self), self._value))
-
-    def __bool__(self) -> bool:
-        return True
+    def __mul__(self, value: _Decimal, /) -> Self:
+        return self.__new__(self.__class__, super().__mul__(value))
 
     @override
-    def __str__(self) -> str:
-        return str(self._value)
+    def __sub__(self, value: _Decimal, /) -> Self:
+        return self.__new__(self.__class__, super().__sub__(value))
 
-    @override
-    def __repr__(self) -> str:
-        return f"{type(self).__name__}({self._value!r})"
+    def __iadd__(self, other: _Decimal, /) -> Self:
+        return self.__add__(other)
 
-    def _ensure_same_type(self, other: object) -> None:
-        if type(self) is not type(other):
-            raise TypeError
+    def __isub__(self, other: _Decimal, /) -> Self:
+        return self.__sub__(other)
 
 
-class Amount(_PositiveDecimal):
-    """Represents a positive physical quantity."""
+class Amount(_AmountABC):
+    """Represents physical amount of smth both in quantitity and weight"""
 
 
-class Money(_PositiveDecimal):
-    """Represents a positive monetary amount."""
+class Money(_AmountABC):
+    """Represents some amount of currency"""
