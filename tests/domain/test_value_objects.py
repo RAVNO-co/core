703069a1911bf29557e843ba4adfebf7d545cf49
BeaverNotACat <beavernotacat@gmail.com>
2026-09-09 12:54:05 +1000

    chore: fix linter errors

--- tests/domain/test_value_objects.py
+++ tests/domain/test_value_objects.py
@@ -6,11 +6,11 @@ from core.domain.exceptions import NegativeOrZeroAmountError
 from core.domain.value_objects import Amount, Money
 
 
-def test_negative_amount_error():
+def test_negative_amount_error() -> None:
     with pytest.raises(NegativeOrZeroAmountError):
         Amount(randint(-5, 0))  # ruff: ignore[suspicious-non-cryptographic-random-usage]
 
 
-def test_negative_money_error():
+def test_negative_money_error() -> None:
     with pytest.raises(NegativeOrZeroAmountError):
         Money(randint(-5, 0))  # ruff: ignore[suspicious-non-cryptographic-random-usage]
